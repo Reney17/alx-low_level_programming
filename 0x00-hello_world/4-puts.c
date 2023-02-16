994c88149dbe827f78f23509409325d078ac89e1
Reney17 <nene171408.gmail.com>
2023-02-16 13:02:19 -0800

    4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.hd>
+/**
+ * main -A scipt that prints a line which puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
