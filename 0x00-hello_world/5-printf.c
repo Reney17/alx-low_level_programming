c1919822608bb838b1dd4981632b8c794ddcf354
Reney17 <nene171408.gmail.com>
2023-02-16 14:10:30 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The program that prints proper grammar
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
