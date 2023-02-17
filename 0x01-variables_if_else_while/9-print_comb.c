aa4aabc4115a5ec2e27794590ad718d5d9227472
Reney17 <nene171408.gmail.com>
2023-02-17 13:46:55 -0800

    print combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,21 @@
 int main(void)
 {
 	/*
-	 * prints the hexadecimal base16,using putchar only
-	 * loops through 9-0,prints them simulating char with arithemtics
-	 * loops through a-f, prints them
+	 * prints all single integers, with comma and space after
+	 * loops through 0-9, prints them,prints comma,print space,
+	 * use only putchar
 	 */
 	int x;
-	char y;
 
 	for (x = 0 ; x < 10 ; x++)
+	{
 		putchar((x % 10) + '0');
-
-	for (y = 'a' ; y <= 'f' ; y++)
-		putchar(y);
+		if (x == 9)
+		/*continue printing comma and space except if x == 9*/
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
-
 	return (0);
 }
