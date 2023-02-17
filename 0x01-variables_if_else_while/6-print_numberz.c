bfd05a0651384228a60c8e2b2d920a6859ee6bb7
Reney17 <nene171408.gmail.com>
2023-02-17 13:06:33 -0800

    numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,12 +3,13 @@
 /**
  * main - to print all single numbers of base 10
  *
- * Return: 0, if succesful
+ * Return: 0(Successful)
  */
 int main(void)
 {
 	int x;
 	/*to print all single numbers of base 10,loop through them*/
+
 	for (x = 0 ; x < 10 ; x++)
 	printf("%d", x);
 	printf("\n");
