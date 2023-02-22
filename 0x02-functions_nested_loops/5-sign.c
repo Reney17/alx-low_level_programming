6354b2a70610b763348e362c0cc44fca0479e9f5
Reney17 <nene171408.gmail.com>
2023-02-21 17:13:21 -0800

    commit changes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,29 @@
-#include "main.h"
+# include "main.h"
 
 /**
- * print_sign - checks for integer sign
- * @n: integer check
+ * print_sign-check the sign of a number.
  *
- * Return: 0 or 1
+ * @n:Number whose sign is to be checked.
+ * Return:1-if n is greater than 0: 0- if n equals 0:-1-if n is less than 0
  */
+
 
 
 int print_sign(int n)
 {
-	if (n < 0)
+	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar(48);
-		return (0);
+		_putchar ('-');
+		return (-1);
 	}
 	else
 	{
-		_putchar(45);
-		return (-1);
+		_putchar('0');
+		return (0);
 	}
 }
