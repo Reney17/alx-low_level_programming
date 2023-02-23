8181a421ad2cd95b55d76527d8db612fb502cffa
Reney17 <nene171408.gmail.com>
2023-02-23 07:14:56 -0800

    multiplies two intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * mul - function declaration
+ * Description: find product of 2 numbers
+ * @a: first number
+ * @b: second number
+ * Return: product
+ */
+
+int mul(int a, int b);
+
+/**
+ * mul - function definition
+ * Description: multiply 2 numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: the product
+ */
+
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
