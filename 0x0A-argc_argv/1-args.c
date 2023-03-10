1523e4db4a10ff14c95662ce1ae3c729c5cf4b46
Reney17 <nene171408.gmail.com>
2023-03-10 08:19:56 -0800

    prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: prints number of command line arguements
+ * @argc: number of arguements
+ * @argv: array of arguemnts
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
