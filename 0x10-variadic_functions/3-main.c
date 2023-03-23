055d8a8849e4d3b786da5ae5d91b881789de3acc
Reney17 <nene171408.gmail.com>
2023-03-23 12:50:37 -0700

    prints anything

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_all("ceis", 'H', 0, "lberton");
+	print_all(NULL);
+	return (0);
+}
