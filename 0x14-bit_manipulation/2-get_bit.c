474cfde50358b8c2e9857e9ab000f479ffe13647
Reney17 <nene171408.gmail.com>
2023-04-07 04:36:34 -0700

    modifying task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,8 +11,14 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > 63)
-		return (-1);
+	unsigned long int k = 1;
 
-	return ((n >> index) & 1);
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	k <<= index;
+
+	return ((n & k) ? 1 : 0);
 }
