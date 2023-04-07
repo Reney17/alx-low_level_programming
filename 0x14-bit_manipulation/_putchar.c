7ab43fa177b88e4f8eca534adc54cfe8d0d8b97a
Reney17 <nene171408.gmail.com>
2023-04-07 03:54:16 -0700

    prints the binary representation of a number

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,12 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * _putchar - prints/writes character
- * @character: The character to print
- * Return: 1, success, -1 error
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-int _putchar(char character)
+int _putchar(char c)
 {
-	return (write(1, &character, 1));
+	return (write(1, &c, 1));
 }
