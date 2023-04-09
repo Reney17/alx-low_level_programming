e70d08e5d51802dc5c840c2d6d42897b8ec533e1
Reney17 <nene171408.gmail.com>
2023-04-09 04:42:40 -0700

    changes to value of a bit at a given index.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,11 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= 64)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	return ((n & (1 << index)) ? 1 : 0);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
 }
